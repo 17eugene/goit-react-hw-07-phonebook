c74e60c808aa0c18fa37f7f32cfe5b8605c3ecb5
17eugene <drivercoua@gmail.com>
2021-10-10 21:35:11 +0300

    Add selectors

--- src/ContactList/ContactList.js
+++ src/ContactList/ContactList.js
@@ -1,12 +1,17 @@
 import { connect } from "react-redux";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getFiltredContacts } from "../redux/contacts/contacts-selectors";
 
 import contactsOperations from "../redux/contacts/contacts-operations";
 import styles from "./ContactList.module.css";
 
-function ContactList({ contacts, onDeleteContact }) {
+function ContactList() {
   const dispatch = useDispatch();
+  const contacts = useSelector(getFiltredContacts);
+
+  const onDeleteContact = (id) =>
+    dispatch(contactsOperations.deleteContact(id));
 
   useEffect(() => {
     dispatch(contactsOperations.getContacts());
@@ -34,26 +39,18 @@ function ContactList({ contacts, onDeleteContact }) {
   );
 }
 
-const getFiltredContacts = (allContacts, filter) => {
-  const normalized = filter.toLowerCase();
+// const mapStateToProps = (state) => {
+//   const visibleContacts = getFiltredContacts(
+//     state.contacts.items,
+//     state.contacts.filter
+//   );
+//   return {
+//     contacts: visibleContacts,
+//   };
+// };
 
-  return allContacts.filter((contact) =>
-    contact.info.name.toLowerCase().includes(normalized)
-  );
-};
+// const mapDispatchToProps = (dispatch) => ({
+//   onDeleteContact: (id) => dispatch(contactsOperations.deleteContact(id)),
+// });
 
-const mapStateToProps = (state) => {
-  const visibleContacts = getFiltredContacts(
-    state.contacts.items,
-    state.contacts.filter
-  );
-  return {
-    contacts: visibleContacts,
-  };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  onDeleteContact: (id) => dispatch(contactsOperations.deleteContact(id)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
+export default connect(null, null)(ContactList);
